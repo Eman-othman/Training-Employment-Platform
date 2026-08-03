6c0210463df991393ee01e350b4bbc3e52f022dd
Eman-othman <eman.busi6842@gmail.com>
2026-08-03 21:34:05 +0300

    Initial commit

---
+++ Visitor-Portal/script.js
@@ -0,0 +1,78 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
+
+// contact section
+
+// MESSAGE INPUT
+const textarea = document.querySelector('.chatbox-message-input')
+const chatboxForm = document.querySelector('.chatbox-message-form')
+
+textarea.addEventListener('input', function () {
+    let line = textarea.value.split('\n').length
+
+    if (textarea.rows < 6 || line < 6) {
+        textarea.rows = line
+    }
+
+    if (textarea.rows > 1) {
+        chatboxForm.style.alignItems = 'flex-end'
+    } else {
+        chatboxForm.style.alignItems = 'center'
+    }
+})
+
+
+
+// TOGGLE CHATBOX
+const chatboxToggle = document.querySelector('.chatbox-toggle')
+const chatboxMessage = document.querySelector('.chatbox-message-wrapper')
+
+chatboxToggle.addEventListener('click', function () {
+    chatboxMessage.classList.toggle('show')
+})
+
+
+
+// DROPDOWN TOGGLE
+const dropdownToggle = document.querySelector('.chatbox-message-dropdown-toggle')
+const dropdownMenu = document.querySelector('.chatbox-message-dropdown-menu')
+
+dropdownToggle.addEventListener('click', function () {
+    dropdownMenu.classList.toggle('show')
+})
+
+document.addEventListener('click', function (e) {
+    if (!e.target.matches('.chatbox-message-dropdown, .chatbox-message-dropdown *')) {
+        dropdownMenu.classList.remove('show')
+    }
+})
+
+
+
+
+
